29da8e6e72d7b0ed9077ba3493758d215e1fffaf
martinstadelmann <m.stadelmann@iqual.ch>
2024-05-16 12:42:44 +0000

    Cleaned up the node creation test

--- tests/src/ExistingSite/NodeCreationTest.php
+++ tests/src/ExistingSite/NodeCreationTest.php
@@ -25,7 +25,7 @@ class NodeCreationTest extends ExistingSiteBase {
    * @throws \Drupal\Core\Entity\EntityMalformedException
    * @throws \Behat\Mink\Exception\ExpectationException
    */
-  public function testLlama() {
+  public function testNodeCreation() {
     // Creates a user. Will be automatically cleaned up at the end of the test.
     $author = $this->createUser([], NULL, TRUE);
 
@@ -42,26 +42,6 @@ class NodeCreationTest extends ExistingSiteBase {
     // We can browse pages.
     $this->drupalGet($node->toUrl());
     $this->assertSession()->statusCodeEquals(200);
-
-    // We can login and browse admin pages.
-    if ($this->loggedInUser) {
-      $this->drupalLogout();
-    }
-
-    $this->drupalGet(Url::fromRoute('user.login'));
-    $this->submitForm([
-      'name' => $author->getAccountName(),
-      'pass' => $author->passRaw,
-    ], t('Log in')->__toString());
-
-    // @see ::drupalUserIsLoggedIn()
-    $author->sessionId = $this->getSession()->getCookie(\Drupal::service('session_configuration')->getOptions(\Drupal::request())['name']);
-    $this->assertTrue($this->drupalUserIsLoggedIn($author), new FormattableMarkup('User %name successfully logged in.', ['%name' => $author->getAccountName()]));
-
-    $this->loggedInUser = $author;
-    $this->container->get('current_user')->setAccount($author);
-
-    $this->drupalGet($node->toUrl('edit-form'));
   }
 
 }
